31abb6be9f66a5b797e9918621a637f2cb6893c2
David Groomes <dwgroomes@gmail.com>
2020-08-24 21:16:45 -0500

    Additional sort examples

--- exec-sort.sh
+++ exec-sort.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-# Examples of "sort"
+# Examples of "sort" and "sort_by"
 
 #echo '[1,3,2]' | jq 'sort'
 
-echo '{"items": [2,1]}' | jq '.items |= sort'
+#echo '{"items": [2,1]}' | jq '.items |= sort'
+
+#echo '{"items": [{"letter": "A", "number": 2}, {"letter": "B", "number": 1}]}' | jq '.items |= sort_by(.number)'
+
+echo '{"B": {"nestedB": null, "nestedA": null}, "A": null}' | jq --sort-keys
